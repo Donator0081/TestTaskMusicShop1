3544519e151cb598dcbc1a548295406999edf51f
Donator0081 <iliya00817@gmail.com>
2020-01-24 17:50:27 +0200

    Update

--- app/src/main/java/com/example/testtaskmusicshop/OrderActivity.java
+++ app/src/main/java/com/example/testtaskmusicshop/OrderActivity.java
@@ -3,10 +3,15 @@ package com.example.testtaskmusicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+    String[] addresses = {"iliya00817@gmail.com"};
+    String subject = "Order from Music shop";
+    String emailText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +25,24 @@ public class OrderActivity extends AppCompatActivity {
         double orderPrice = receiverOrderIntent.getDoubleExtra("orderPrice", 0);
         double price = receiverOrderIntent.getDoubleExtra("price", 0);
 
-        TextView textViewForOrder = findViewById(R.id.textViewForOrder);
-        textViewForOrder.setText("Customer name: " + userName + "\n"
+        emailText="Customer name: " + userName + "\n"
                 + "Goods name: " + goodsName + "\n"
                 + "Quantity: " + quantity + "\n"
                 + "Price: " + price + "\n"
-                + "Order price: " + orderPrice);
+                + "Order price: " + orderPrice;
+        TextView textViewForOrder = findViewById(R.id.textViewForOrder);
+        textViewForOrder.setText(emailText);
+    }
+
+    public void sendToEmail(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
 }
